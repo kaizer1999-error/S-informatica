90330690dc0e381cdc1b1d999fae75742348e359
kaizer <bbb@vv.>
2025-09-26 17:06:53 +0300

    hola

---
+++ boolean.js
@@ -0,0 +1,5 @@
+const is_expression_valid = 4*8%3
+const result_of_comprasion = 8-345
+const expression_check = is_expression_valid > result_of_comprasion
+
+console.log(expression_check)
---
+++ var.js
@@ -0,0 +1,4 @@
+const firstNumber = 7
+const secondNumer = 8
+const result = firstNumber + secondNumer 
+console.log(result)
